f78f95cbc2e7a5479d4117f2e4f0a4b2b5767aaa
wolbyte <wolbyte@gmail.com>
2025-12-03 19:44:03 +0330

    add sql and proto language support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
 
     -- import/override with your plugins
